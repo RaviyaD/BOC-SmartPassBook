2955f1eed032b4025f64dd240e972fc83109d970
Dissanayaka, Raveen <raveen.dissanayaka@pearson.com>
2020-10-10 16:01:35 +0530

    Version 2.0 - login

--- Intents/Login.js
+++ Intents/Login.js
@@ -7,7 +7,7 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
-    ImageBackground, Image,StatusBar
+    ImageBackground, Image, StatusBar
 } from 'react-native';
 import * as firebase from "firebase";
 import NetInfo from "@react-native-community/netinfo";
@@ -20,7 +20,7 @@ class Login extends Component {
         usernameError: '',
         isVisible: true,
         loginList: [],
-        isConnected : false
+        isConnected: false
     };
 
     componentDidMount() {
@@ -29,9 +29,9 @@ class Login extends Component {
             that.Hide_Splash_Screen();
         }, 3000);
 
-      setInterval(() =>  NetInfo.addEventListener(state => {
-            this.setState({isConnected : state.isConnected})
-        }),2000);
+        setInterval(() => NetInfo.addEventListener(state => {
+            this.setState({isConnected: state.isConnected})
+        }), 2000);
 
         firebase.database().ref('Login').on('value', (snapshot) => {
             snapshot.forEach((item) => {
@@ -55,7 +55,7 @@ class Login extends Component {
     login = (email, pass) => {
         let login = this.validateUsername();
         //if (login)
-            this.props.navigation.navigate('Dashboard');
+        this.props.navigation.navigate('Dashboard');
     };
     toRegisterIntent = (email, pass) => {
         this.props.navigation.navigate('Register');
@@ -91,39 +91,55 @@ class Login extends Component {
                        style={{width: '100%', height: '100%'}}/>
             </View>);
         let loginView = (
-            <ImageBackground style={styles.backgroundImage} source={require('../assets/common.jpg')}>
-                <Text style={styles.bocText}>BOC</Text>
-                <Text style={styles.passBookText}>Smart | PassBook</Text>
+            <View style={styles.container}>
+                <View style={styles.topicBackground}>
+                    <Image
+                        style={styles.bocImage}
+                        source={require('../assets/logoBOC.png')}
+                    />
+                    <View style={styles.innerText}>
+                        <Text style={styles.passBookText}>Smart | PassBook</Text>
+                    </View>
 
-                <Text style={styles.loginText}>Login</Text>
-                <Text style={styles.inputUn}>Username</Text>
-                <TextInput style={styles.input}
-                           underlineColorAndroid="transparent"
-                           autoCapitalize="none"
-                           onChangeText={this.handleEmail}/>
+                </View>
+
+                <ImageBackground source={require('../assets/main.jpg')} style={styles.body}>
 
 
-                <Text style={styles.inputPw}>Password</Text>
-                <TextInput style={styles.input}
-                           underlineColorAndroid="transparent"
-                           autoCapitalize="none"
-                           type='password'
-                           secureTextEntry={true}
-                           onChangeText={this.handlePassword}/>
-                <Text style={styles.errorMsg}>{this.state.usernameError}</Text>
-                <TouchableOpacity
-                    style={styles.submitButton}
-                    onPress={
-                        () => this.login(this.state.email, this.state.password)
-                    }>
-                    <Text style={styles.submitButtonText}> Submit </Text>
-                </TouchableOpacity>
-                <Text style={styles.registerLink} onPress={() => this.toRegisterIntent()}>Register ?</Text>
-            </ImageBackground>
+                    <Text style={styles.inputUn}>Username</Text>
+                    <TextInput style={styles.input}
+                               underlineColorAndroid="transparent"
+                               autoCapitalize="none"
+                               onChangeText={this.handleEmail}/>
+
+
+                    <Text style={styles.inputPw}>Password</Text>
+                    <TextInput style={styles.input}
+                               underlineColorAndroid="transparent"
+                               autoCapitalize="none"
+                               type='password'
+                               secureTextEntry={true}
+                               onChangeText={this.handlePassword}/>
+                    <Text style={styles.errorMsg}>{this.state.usernameError}</Text>
+                    <TouchableOpacity
+                        style={styles.submitButton}
+                        onPress={
+                            () => this.login(this.state.email, this.state.password)
+                        }>
+                        <Text style={styles.submitButtonText}>L O G I N</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.registerLink} onPress={() => this.toRegisterIntent()}>Register ?</Text>
+
+                        <Image style={styles.bottomView} source={require('../assets/bocBottom.png')}/>
+
+                </ImageBackground>
+
+            </View>
         )
         return (
             <View style={styles.container}>
-                { !this.state.isConnected ? <View style={{width:'100%',height:25,backgroundColor: 'red'}}><Text style={{textAlign:'center'}}>No Internet Connection</Text></View> : null }
+                {!this.state.isConnected ? <View style={{width: '100%', height: 25, backgroundColor: 'red'}}><Text
+                    style={{textAlign: 'center'}}>No Internet Connection</Text></View> : null}
                 {
                     (this.state.isVisible === true) ? Splash_Screen : loginView
                 }
@@ -138,6 +154,31 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    bocImage: {
+        marginTop: 10,
+        height: 60,
+        width: 150
+    },
+    topicBackground: {
+        textAlign: 'center',
+        alignItems: 'center',
+        width: '100%',
+        // resizeMode: 'center',
+
+        shadowOffset: {width: 10, height: 10},
+        shadowColor: '#000',
+        shadowOpacity: 1,
+        elevation: 10,
+        backgroundColor: "#FDC723"
+    },
+    innerText: {
+        marginBottom: 10
+    },
+    body: {
+        alignItems: 'center',
+        height: '100%',
+
+    },
     errorMsg: {
         color: '#800000',
     },
@@ -146,11 +187,12 @@ const styles = StyleSheet.create({
         fontSize: 60,
         textAlign: 'center',
         textShadowColor: '#000000',
-        color: '#ffd800',
+        color: '#FDDD00',
         textShadowOffset: {width: 3, height: 3},
         textShadowRadius: 10,
     },
     passBookText: {
+        marginTop: 0,
         fontWeight: 'bold',
         fontSize: 30,
         textAlign: 'center',
@@ -158,7 +200,7 @@ const styles = StyleSheet.create({
     },
     loginText: {
         fontWeight: 'bold',
-        fontSize: 30,
+        fontSize: 32,
         textAlign: 'center',
         fontFamily: 'sans-serif-condensed',
         paddingTop: 30,
@@ -166,38 +208,47 @@ const styles = StyleSheet.create({
     inputUn: {
         textAlign: 'center',
         paddingTop: 20,
-        fontSize: 15,
+        fontSize: 17,
+        marginTop:50
     },
     inputPw: {
         textAlign: 'center',
         paddingTop: 20,
-        fontSize: 15,
+        fontSize: 17,
+        marginTop:10
     },
     input: {
-        margin: 15,
-        width: 200,
+        margin: 5,
+        width: 230,
         height: 40,
         borderColor: '#000000',
         borderRadius: 10,
         textAlign: 'center',
         borderWidth: 1,
+        backgroundColor:'white'
+
     },
     submitButton: {
-        backgroundColor: '#000000',
+        backgroundColor: '#FDC722',
         padding: 10,
-        margin: 15,
+        alignSelf: 'center',
+        margin: 10,
         height: 40,
-        width: 150,
+        width: 180,
         borderRadius: 10,
+
     },
     submitButtonText: {
+        fontWeight: 'bold',
         textAlign: 'center',
-        color: 'white',
+        color: 'black',
+        fontSize:15
     },
     registerLink: {
         textAlign: 'center',
         textDecorationLine: 'underline',
         fontWeight: 'bold',
+        fontSize:15
     },
     logo: {
         textAlign: 'center',
@@ -207,9 +258,12 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%',
         resizeMode: 'stretch',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
+    bottomView :{
+        marginTop:100,
+        width:250,
+        height:25
+    }
 });
 
 export default Login;
